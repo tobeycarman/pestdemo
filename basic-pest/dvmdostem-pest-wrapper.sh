2e591f8ab9a593d1a30976c7e346c9c3c6a81dcb
Tobey Carman <tcarman2@alaska.edu>
2016-01-08 14:22:21 +0000

    update wrapper script

--- basic-pest/dvmdostem-pest-wrapper.sh
+++ basic-pest/dvmdostem-pest-wrapper.sh
@@ -4,11 +4,12 @@
 
 # Run the model
 cd ~/dvm-dos-tem/
-./dvmdostem --cal-mode -l note -p 10 -m 100
+./dvmdostem --cal-mode -l note -p 10 -m 10
 
 # Create the simplified output 
 cd ~/pestdemo/
-rm -f basic-pest/simplified-output.csv
-./json2simpletxt.py basic-pest/simplified-output.csv
+rm -f basic-pest/simplified-outputs.txt
+./json2simpletxt.py basic-pest/simplified-outputs.txt
+
 
 
