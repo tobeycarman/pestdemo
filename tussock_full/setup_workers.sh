79cf2b41a6499e0020d4d3d003076b389da87935
Tobey Carman <tcarman2@alaska.edu>
2016-08-02 23:23:22 -0700

    rename worker zero to master zero,
    add corresponding delete and create code, and
    add the step to setup the master pestpp process

--- tussock_full/setup_workers.sh
+++ tussock_full/setup_workers.sh
@@ -13,14 +13,16 @@ usage () {
        -h, --help  Show this message and quit.
        --list      Show info about any existing worker directories
        --cleanup   The program will delete all directories and log files matching
-                   '$HOME/wrkr-*' and '$HOME/wrkr-*.log'
+                   '$HOME/wrkr-*', '$HOME/master-*' and '$HOME/wrkr-*.log' and '$HOME/master-*.log'
        --start     The program will start a worker in each directory matching '$HOME/wrkr-*'
        NWORKERS     Program will create N directories starting with '$HOME/wrkr-00000'
   "
 }
 
 list_workers() {
-  find $HOME -type d -name "wrkr-*" 
+  find $HOME -type d -name "master-*"
+  find $HOME -type d -name "wrkr-*"
+
 }
 
 cleanup () {
@@ -37,7 +39,20 @@ cleanup () {
     echo "Removed '$log_file'."
   done
 
+  find $HOME -type d -name "master-*" -print0 2>/dev/null| while IFS= read -r -d '' master_dir
+  do
+    rm -rf $master_dir
+    echo "Removed '$master_dir'"
+  done
+
+  find $HOME -type f -name "master-*.log" -print0 2>/dev/null | while IFS= read -r -d '' master_log
+  do
+    rm $master_log
+    echo "Removed '$master_log'"
+  done
+
 }
+
 
 setup_workers() {
 
@@ -52,7 +67,7 @@ setup_workers() {
     exit -1
   fi
 
-  echo "Will try creating directory structure for $NWORKERS..."
+  echo "Will attempt to create directory structure for $NWORKERS workers and one master..."
   if [[ $NWORKERS -gt 25 ]]
   then
     echo "I refuse. Thats too many directories to create!"
@@ -76,15 +91,30 @@ setup_workers() {
     cp ~/pestdemo/tussock_full/pest-helper.py $FULL_SPATH
 
   done
+
+  mv "$HOME/wrkr-00000" "$HOME/master-00000"
 }
 
 start_workers() {
+
+  find $HOME -type d -name "master-*" -print0 | while IFS= read -r -d '' master_dir
+  do
+    echo "$master_dir"
+    echo "$HOME/master-00000"
+    # if [[ "$master_dir" -ne  "$HOME/master-00000" ]]
+    # then
+    #   echo "Something is wrong! There should not be any other master directories present!"
+    #   exit -1
+    # fi
+    BN=$(basename "$master_dir")
+    cd "$master_dir" && pestpp tussock_full.pst /H :5050 > "$HOME/$BN.log" 2>&1 &
+  done
 
   # look for wrkr-* directories
   find $HOME -type d -name "wrkr-*" -print0 |  while IFS= read -r -d '' worker_dir
   do
     BN=$(basename "$worker_dir")
-    cd "$worker_dir" && pestpp tussock_full.pst/H :5050 > "$HOME/$BN.log" 2>&1 &
+    cd "$worker_dir" && pestpp tussock_full.pst /H localhost:5050 > "$HOME/$BN.log" 2>&1 &
     PID=$!
     echo "Started worker with pid=$PID in: '$worker_dir'"
   done
