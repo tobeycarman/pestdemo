c39992c26e6617c887fdf17648c25645de30d671
Tobey Carman <tobeycarman@gmail.com>
2016-08-26 13:01:25 -0800

    Change wrapper script to use /atlas_scratch

--- tussock_full/dvmdostem-pest-wrapper.sh
+++ tussock_full/dvmdostem-pest-wrapper.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-/atlas_scratch/$USER/dvm-dos-tem/dvmdostem -l fatal --cal-mode -p 100 -m 1000
+WORKING_ROOT="/atlas_scratch/$USER"
+
+$WORKING_ROOT/dvm-dos-tem/dvmdostem -l err --cal-mode -p 100 -m 1000 -s 0 -t 0 -n 0
 
 rm -f simplified-outputs.txt
 ./pest-helper.py --json-to-obs simplified-outputs.txt --json-location="why-here" 
