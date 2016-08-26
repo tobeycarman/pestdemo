e0a3f38e81bd777b30dec06ae158e3a8f12e4380
Tobey Carman <tobeycarman@gmail.com>
2016-08-26 13:00:42 -0800

    update slurm script.
    THis still was really finicky to get right. Seems to help to explicitly
    specify the nodes to use.

--- tussock_full/slurm-job.sh
+++ tussock_full/slurm-job.sh
@@ -61,25 +61,35 @@
 #      and delete all the worker (and master) directories with the setup-workers.sh --cleanup
 #  
 
-#SBATCH -n 74               # cores 
-#SBATCH -t 1-03:00:00       # 1 day and 3 hours
+#SBATCH -n 73                # cores
+#SBATCH -t 1-13:00:00       # 1 day and 13 hours
 #SBATCH --partition main    # The partition 
-#SBATCH -J tbc_test_pestpp  # sensible name for the job
+#SBATCH -J pestpp-cal       # sensible name for the job
+#SBATCH -w atlas13,atlas14,atlas15
+MASTERNODE="atlas13"
+NWORKERS=73
+WORKING_ROOT="/atlas_scratch/$USER"
 
+# Deal with the master process
+echo "Starting master process..."
+cd $WORKING_ROOT/master-00000 
+srun --partition main --nodelist="atlas13" -n 1 pestpp tussock_full.pst /H :5050 > $WORKING_ROOT/master-00000.log 2>&1 &
+echo "The srun exit status was: $?"
+echo "Created the master process..."
 
-cd $HOME/master-00000 
-srun --partition main --nodelist="atlas11" -n 1  pestpp tussock_full.pst /H :5050 > $HOME/master-00000.log 2>&1 &
+# Wait, seems to help with making sure workers can connect...
+DELAY=30
+echo Start: $(date)
+echo "Waiting $DELAY second for the master to start before starting workers..."
+sleep $DELAY
+echo End wait: $(date)
 
-echo "Waiting for the master to start before starting workers..."
-sleep 30
-
-NWORKERS=74
-
+# Start all the workers...
 for (( i=1; i<=$NWORKERS; ++i ))
 do
   printf -v ZPNUM "%05d" $i # Zero padded number...
-  cd $HOME/wrkr-$ZPNUM &&
-  srun -n 1 --partition main pestpp tussock_full.pst /H atlas11:5050 > $HOME/wrkr-$ZPNUM.log 2>&1 &
+  cd $WORKING_ROOT/wrkr-$ZPNUM &&
+  srun -n 1 --partition main pestpp tussock_full.pst /H atlas13:5050 > $WORKING_ROOT/wrkr-$ZPNUM.log 2>&1 &
   echo "Started worker number $ZPNUM..."
 done
 
