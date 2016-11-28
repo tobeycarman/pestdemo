e520b2f5ca593537f97bd1ff61e54766155084ff
Tobey Carman <tobeycarman@gmail.com>
2016-11-28 12:08:41 -0900

    Change so that the wrapper script is looking for the pest-helper.py script up a directory.

--- tussock_full/dvmdostem-pest-wrapper.sh
+++ tussock_full/dvmdostem-pest-wrapper.sh
@@ -5,4 +5,4 @@ WORKING_ROOT="/atlas_scratch/$USER"
 $WORKING_ROOT/dvm-dos-tem/dvmdostem -l err --cal-mode --last-n-json 10 -p 100 -e 1000 -s 0 -t 0 -n 0
 
 rm -f simplified-outputs.txt
-./pest-helper.py --json-to-obs simplified-outputs.txt --json-location="why-here" 
+../pest-helper.py --json-to-obs simplified-outputs.txt --json-location="why-here" 
